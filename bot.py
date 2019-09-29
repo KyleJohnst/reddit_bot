82be64386158c621fa53a7ed3d15490695ae5e49
Kyle Johnston <kyle9657@gmail.com>
2019-09-29 22:22:43 +0100

    Added in try except logic and changed DAN link to the internalional page

--- bot.py
+++ bot.py
@@ -23,17 +23,20 @@ values = []
 for keyword in open('keywords.txt'):
     values.append(keyword.strip('\n'))
 
-for submission in reddit.subreddit('scuba').stream.submissions():
+for submission in reddit.subreddit('KyleDevEnv').stream.submissions():
     print(submission.title)
 
+try:
     if any(word in submission.title.lower() or word in submission.selftext.lower() for word in values):
         if submission.id not in posts_replied_to:
                 # Reply
-                submission.reply("If you have any medical concerns in regards to diving, you should contact [DAN](https://www.diversalertnetwork.org/) for medical advice. The opinions here should not supersede that of a trained medical practioner." + "\n\n\n\n---\n\n^(Beep boop. I am a baby bot and still make mistakes. If there are any issues, contact my) [^Master ](https://www.reddit.com/message/compose/?to=Aer0za&subject=/u/DiveBotto)\n\n^(Check out my ) [^GitHub ](https://github.com/KyleJohnst/reddit_bot)")
+                submission.reply("If you have any medical concerns in regards to diving, you should contact [DAN](http://danap.org/inside/hotlines.php) for medical advice. The opinions here should not supersede that of a trained medical practioner." + "\n\n\n\n---\n\n^(Beep boop. I am a baby bot and still make mistakes . If there are any issues, contact my) [^Master ](https://www.reddit.com/message/compose/?to=Aer0za&subject=/u/DiveBotto)\n\n^(Check out my ) [^GitHub ](https://github.com/KyleJohnst/reddit_bot)")
                 print("Bot replying to : ", submission.title)
 
                 # Store id in response list
                 posts_replied_to.append(submission.id)
+except Exception as exc:
+    print("An error occured: ", exc)
 
 # Write updated list to file
 with open("posts_replied_to.txt", "w") as f:
