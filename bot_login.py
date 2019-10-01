901bd1ba27c389d3968aa22294016ac3e689644d
Kyle Johnston <kyle9657@gmail.com>
2019-10-01 14:28:04 +0100

    Moved login to separate file commented code

---
+++ bot_login.py
@@ -0,0 +1,16 @@
+import praw
+import os
+
+def bot_login():
+    print("Logging in...")
+    try:
+        reddit_instance = praw.Reddit(username=os.environ["username"],
+                             password=os.environ["password"],
+                             client_id=os.environ["client_id"],
+                             client_secret=os.environ["client_secret"],
+                             user_agent=os.environ["user_agent"])
+        # reddit_instance = praw.Reddit('bot1')
+        print("Logged in!")
+    except:
+        print("Failed to log in!")
+    return reddit_instance
