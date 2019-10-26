f129114ee69554309f6ee40fee6f7d64e7efe44f
Kyle Johnston <kyle9657@gmail.com>
2019-10-26 14:00:07 +0100

    Added in calculation for max EaNx percentage for a given dept that responds to comments

--- bot.py
+++ bot.py
@@ -24,9 +24,27 @@ for keyword in open('keywords.txt'):
 currentDT = time.time()
 TimeLim = currentDT - 3600.00 # 1 Hour limit
 
+def eanxCalc(param):
+    try:
+        measurement = param[-1]
+        if measurement == 'm':
+            x = 1.4/(int(param[0])/10+1)
+            eanx = x * 100
+            return int(eanx)
+        elif measurement == 'ft':
+            conv = int(param[0])/3.2808
+            x = 1.4/(conv/10+1)
+            eanx = x * 100
+            return int(eanx)
+    except Exception as exc:
+        print("Failed nitrox percentage calculation with exc: ", exc)
+
+
+botTag = "\n\n\n\n---\n\n^(Beep boop. I am a baby bot and still make mistakes . If there are any issues, contact my) [^Master ](https://www.reddit.com/message/compose/?to=Aer0za&subject=/u/DiveBotto)\n\n^(Check out my ) [^GitHub ](https://github.com/KyleJohnst/reddit_bot)"
+
 try:
     # Monitor new posts stream
-    for submission in reddit.subreddit(os.environ["subreddit"]).stream.submissions():
+    for submission in reddit.subreddit(os.environ["KyleDevEnv"]).stream.submissions():
         print(submission.title)
 
         # Ensure bot only responds to posts from last 12 hours
@@ -38,15 +56,31 @@ try:
             if any(word in header or word in content for word in values):
                 if submission.id not in posts_replied_to:
                     # Reply
-                    submission.reply("If you have any medical concerns in regards to diving accidents or emergencies, you should contact [DAN Emergency Numbers](http://danap.org/inside/hotlines.php)." + "\n\n\n For non-emergency communication use these links below" + "\n\n\n [DAN North America](https://www.diversalertnetwork.org/contact/)" + "\n\n [DAN Brasil](https://www.danbrasil.org.br/contescritorio)" +  "\n\n [DAN Europe](https://www.daneurope.org/web/guest/contacts#p_p_id_56_INSTANCE_l42C_)" + "\n\n [DAN Southern Africa](https://www.dansa.org/contact)" + "\n\n [DAN Asia Pacific](http://danap.org/contact_dan/asiapacificoffices.php)" + "\n\n [DAN Japan](https://www.danjapan.gr.jp/form/contact)" + "\n\n [DAN World](http://world.dan.org/contact-danworld)" + "\n\n The opinions of members here should not supersede that of a trained medical practioner." + "\n\n\n\n---\n\n^(Beep boop. I am a baby bot and still make mistakes . If there are any issues, contact my) [^Master ](https://www.reddit.com/message/compose/?to=Aer0za&subject=/u/DiveBotto)\n\n^(Check out my ) [^GitHub ](https://github.com/KyleJohnst/reddit_bot)")
+                    submission.reply("If you have any medical concerns in regards to diving accidents or emergencies, you should contact [DAN Emergency Numbers](http://danap.org/inside/hotlines.php)." + "\n\n\n For non-emergency communication use these links below" + "\n\n\n [DAN North America](https://www.diversalertnetwork.org/contact/)" + "\n\n [DAN Brasil](https://www.danbrasil.org.br/contescritorio)" +  "\n\n [DAN Europe](https://www.daneurope.org/web/guest/contacts#p_p_id_56_INSTANCE_l42C_)" + "\n\n [DAN Southern Africa](https://www.dansa.org/contact)" + "\n\n [DAN Asia Pacific](http://danap.org/contact_dan/asiapacificoffices.php)" + "\n\n [DAN Japan](https://www.danjapan.gr.jp/form/contact)" + "\n\n [DAN World](http://world.dan.org/contact-danworld)" + "\n\n The opinions of members here should not supersede that of a trained medical practioner." + botTag)
                     print("Bot replying to : ", submission.title)
                     # Store id in response list
                     posts_replied_to.append(submission.id)
-        else:
-            print("Posted EXCEEDED new post time limit and wont be responded to.")
 
 except Exception as exc:
-    pass
+    print("Process failed to respond to new submissions with exc: ", exc)
+
+try:
+    for comment in reddit.subreddit('KyleDevEnv').stream.comments():
+            if comment.created_utc > TimeLim:
+                context = comment.body.lower().split(' ')
+                for keyword in context:
+                    if keyword == 'nitrox%':
+                        parameter = context.pop(1)
+                        params = parameter.split('/')
+                        eanx = str(eanxCalc(params))
+                        if eanx != 'None':
+                            comment.reply("Hello, the maximum O^2 percentage for your dive to a maximum depth of" + params[0] + params[-1] + ", would be " + eanx + "%. Rounded down for conservatism with a PO^2 of 1.4" + "\n\n\n This is just a guidline and should not replace your own calculations" + botTag)
+                            print("Replying with max O2 percentage to: ", comment.body)
+                        else:
+                            comment.reply("Hello, you will need to specify wether I need to work from feet or meters, please call again using the format of max_depth/ft or max_depth/m" + botTag)
+                            print("Educating person: ", comment.body)
+
+except Exception as exc:
     print("Process failed with exc: ", exc)
 
 # Write updated list to file
--- bot_login.py
+++ bot_login.py
@@ -4,12 +4,12 @@ import os
 def bot_login():
     print("Logging in...")
     try:
-        reddit_instance = praw.Reddit(username=os.environ["username"],
-                             password=os.environ["password"],
-                             client_id=os.environ["client_id"],
-                             client_secret=os.environ["client_secret"],
-                             user_agent=os.environ["user_agent"])
-        # reddit_instance = praw.Reddit('bot1')
+        # reddit_instance = praw.Reddit(username=os.environ["username"],
+        #                      password=os.environ["password"],
+        #                      client_id=os.environ["client_id"],
+        #                      client_secret=os.environ["client_secret"],
+        #                      user_agent=os.environ["user_agent"])
+        reddit_instance = praw.Reddit('bot1')
         print("Logged in!")
     except:
         print("Failed to log in!")
